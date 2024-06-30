7d5a11db7661bf05c3d9b246ac39c74be762c12a
Trey Smith <slicksmith14@gmail.com>
2024-06-30 14:10:35 -0400

    Add input funtionality to the gameboard and indexing each gameboard spot for the array

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 // Gameboad object
 const gameboard = (function() {
     //create gameboard array
-    let board = []
+    let board = [];
     for (let i=0; i<9; i++) {
         board.push("");
     }
+    // Indexing for gameboard spots
+    let index = 0;
+    const getIndex = () => index;
+    const increaseIndex = () => index++;
+    const resetIndex = () => index = 0;
+    return {getIndex, increaseIndex, resetIndex};
 })();
 
+// Handles input
+document.querySelectorAll(".spot").forEach((spot) => {
+    spot.addEventListener('click', clickSpot, {once:true})
+    spot.setAttribute("id", gameboard.getIndex());
+    gameboard.increaseIndex();
+
+    function clickSpot() {
+        spot.innerHTML="CLICK";
+    }
+})
