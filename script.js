ebb91deff941540fa7c733ebf238c80c7a6789a8
Trey Smith <slicksmith14@gmail.com>
2024-07-04 15:22:23 -0400

    Add more functionality to the game

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-// Gameboad object
+//Gameboad Object
 const gameboard = (function() {
-    //create gameboard array
+    //Creates gameboard array
     let board = [];
+
     for (let i=0; i<9; i++) {
         board.push("");
     }
-    // Indexing for gameboard spots
+    //Sets and Gets gameboard values
+    const setBoard = (index, val) => (board[index] = val);
+    const getBoard = () => board;
+    //Indexing for gameboard spots
     let index = 0;
     const getIndex = () => index;
     const increaseIndex = () => index++;
     const resetIndex = () => index = 0;
-    return {getIndex, increaseIndex, resetIndex};
+    return {getIndex, increaseIndex, resetIndex, setBoard, getBoard};
+
 })();
 
-// Handles input
+//Game Object
+const game = (function() {
+    //Creates both players
+    const playerOne = createPlayer("Player X", "X");
+    const playerTwo = createPlayer("Player O", "O");
+    //Handles player turns
+    let tick = 0;
+    const getTick = () => tick;
+    const increaseTick = () => tick++;
+    const resetTick = () => tick = 0;
+    return {getTick, increaseTick, resetTick};
+})();
+
+//Creates players
+function createPlayer(name, marker) {
+    return {name, marker};
+}
+
+const checkWinner = (function() {
+
+})
+
+//Handles input
 document.querySelectorAll(".spot").forEach((spot) => {
     spot.addEventListener('click', clickSpot, {once:true})
     spot.setAttribute("id", gameboard.getIndex());
     gameboard.increaseIndex();
 
     function clickSpot() {
-        spot.innerHTML="CLICK";
+        let temp = game.getTick();
+        if (temp %= 2){
+            gameboard.setBoard(spot.getAttribute("id"), "O");
+            spot.innerHTML = "O";
+            game.increaseTick();
+        }
+        else {
+            gameboard.setBoard(spot.getAttribute("id"), "X");
+            spot.innerHTML = "X";
+            game.increaseTick();
+        }
+
+        console.log(gameboard.getBoard());
     }
 })
