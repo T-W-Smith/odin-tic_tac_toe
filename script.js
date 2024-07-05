49c8cdccf999c7ecf36be59625fdff1531f1238b
Trey Smith <slicksmith14@gmail.com>
2024-07-04 20:26:27 -0400

    Add winning conditions and check for a winner

--- script.js
+++ script.js
@@ -37,28 +37,60 @@ function createPlayer(name, marker) {
 }
 
 const checkWinner = (function() {
+    const winningCondition = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-})
+    let winner = false;
+    const getWinner = () => winner;
+    
+    function validateWinner() {
+        for (let i = 0; i <= 7; i++) {
+            const condition = winningCondition[i];
+            let one = gameboard.getBoard()[condition[0]];
+            let two = gameboard.getBoard()[condition[1]];
+            let three = gameboard.getBoard()[condition[2]];
+            if (one === '' || two === '' || three === '') {
+                continue;
+            }
+
+            if (one === two && two === three) {
+                winner = true;
+                break
+            }
+        }
+    }
+    return {validateWinner, getWinner};
+})();
 
 //Handles input
 document.querySelectorAll(".spot").forEach((spot) => {
-    spot.addEventListener('click', clickSpot, {once:true})
+    spot.addEventListener('click', clickSpot, {once:true});
     spot.setAttribute("id", gameboard.getIndex());
     gameboard.increaseIndex();
 
     function clickSpot() {
-        let temp = game.getTick();
-        if (temp %= 2){
-            gameboard.setBoard(spot.getAttribute("id"), "O");
-            spot.innerHTML = "O";
-            game.increaseTick();
-        }
-        else {
-            gameboard.setBoard(spot.getAttribute("id"), "X");
-            spot.innerHTML = "X";
-            game.increaseTick();
-        }
+        if (!checkWinner.getWinner()) {
+                let temp = game.getTick();
+                if (temp %= 2){
+                    gameboard.setBoard(spot.getAttribute("id"), "O");
+                    spot.innerHTML = "O";
+                    game.increaseTick();
+                }
+                else {
+                    gameboard.setBoard(spot.getAttribute("id"), "X");
+                    spot.innerHTML = "X";
+                    game.increaseTick();
+                }
+            }
 
-        console.log(gameboard.getBoard());
+        checkWinner.validateWinner();
     }
 })
